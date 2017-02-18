c906b91085c7866c882ec1a9e1e9b22a00687a4d
d.voitekh <d.voitekh@gmail.com>
2017-02-18 21:30:03 +0200

    first release

--- main.js
+++ main.js
@@ -18,7 +18,10 @@ function createWindow () {
   }));
 
   // Open the DevTools.
-  win.webContents.openDevTools();
+  // win.webContents.openDevTools();
+  win.webContents.on('devtools-opened', () => {
+    win.webContents.closeDevTools();
+  });
 
   // Emitted when the window is closed.
   win.on('closed', () => {
--- renderer/components/main.js
+++ renderer/components/main.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default class Main extends React.Component {
   render() {
+    let divStyle = {textAlign: 'center', padding: '10px 20px'};
+    let imgStyle = {width: '400px'};
     return (
-      <h1>Hello world</h1>
+      <div style={divStyle}>
+        <img style={imgStyle} src={'https://s-media-cache-ak0.pinimg.com/originals/7c/ea/d8/7cead8235b46453b0777ef08d8fbd96b.jpg'} />
+        <h1>Hello Electron</h1>
+      </div>
     );
   }
 }
