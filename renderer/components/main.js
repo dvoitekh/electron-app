abdbfbd5368e2d43ccbf75a5715c70c11a563853
d.voitekh <d.voitekh@gmail.com>
2017-02-19 23:01:36 +0200

    added data persistence via redis client

--- renderer/components/main.js
+++ renderer/components/main.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import redis from 'redis';
+
+let client = redis.createClient();
 
 export default class Main extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      items: ['hello', 'bye', 'love you'],
+      items: [],
       newItem: ''
     };
+
+    client.lrange('items', 0, -1, (err, items) => {
+      this.setState({
+        items: items,
+      });
+    });
   }
 
-  handleClick() {
+  persistData(newItems) {
     this.setState({
-      items: [this.state.newItem, ...this.state.items],
+      items: newItems,
       newItem: ''
     });
+
+    client.del('items');
+    client.rpush.apply(client, ['items'].concat(newItems));
+  }
+
+  handleAddition() {
+    this.persistData([this.state.newItem, ...this.state.items]);
+  }
+
+  handleDeletion(index, e) {
+    this.persistData([...this.state.items.slice(0, index), ...this.state.items.slice(index + 1)]);
   }
 
   handleChange(e) {
@@ -28,10 +48,15 @@ export default class Main extends React.Component {
     return (
       <div style={divStyle}>
         <input className="" type="text" placeholder="Enter new To Do" value={this.state.newItem} onChange={this.handleChange.bind(this)} />
-        <input type="button" value="Submit" onClick={this.handleClick.bind(this)} />
+        <input type="button" value="Submit" onClick={this.handleAddition.bind(this)} />
         <ul>
           {this.state.items.map((item, i) => {
-            return <li key={i}>{item}</li>;
+            return (
+              <li key={i}>
+                {item}
+                <input type="button" value="Delete" onClick={this.handleDeletion.bind(this, i)} />
+              </li>
+            );
           })}
         </ul>
       </div>
