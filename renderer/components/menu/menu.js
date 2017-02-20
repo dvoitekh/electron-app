bf8f4e1ffd4035044aedbc3f3b062094c573c350
d.voitekh <d.voitekh@gmail.com>
2017-02-20 15:29:54 +0200

    some style improvements

--- renderer/components/menu/menu.js
+++ renderer/components/menu/menu.js
@@ -4,10 +4,13 @@ import { Link } from 'react-router';
 export default class Menu extends Component {
   render() {
     return (
-      <ul className="main-nav">
-        <Link className="menu-item" to="/">Home</Link>
-        <Link className="menu-item" to="/graph">Graph</Link>
-      </ul>
+      <div className="nav-container">
+        <div className="fake-nav"></div>
+        <ul className="main-nav">
+          <Link className="menu-item" to="/" activeClassName="active">Home</Link>
+          <Link className="menu-item" to="/graph" activeClassName="active">Graph</Link>
+        </ul>
+      </div>
     );
   }
 }
