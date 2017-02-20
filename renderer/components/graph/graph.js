8ecfeaa72f2517b9c31ce88687cf29f4444af8d7
d.voitekh <d.voitekh@gmail.com>
2017-02-20 13:56:44 +0200

    chartjs

--- renderer/components/graph/graph.js
+++ renderer/components/graph/graph.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import { Line as LineChart } from 'react-chartjs';
 
-export default class Main extends Component {
+let chartData =  {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+  }]
+};
+
+let chartOptions = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero:true
+      }
+    }]
+  }
+};
+
+export default class Graph extends Component {
   render() {
     return (
-      <h1>Graph</h1>
+      <div>
+        <h1>Graph</h1>
+        <LineChart data={chartData} options={chartOptions} width="600" height="200"/>
+      </div>
     );
   }
 }
